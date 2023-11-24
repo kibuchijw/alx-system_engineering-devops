e6cfc29972c4c5f2410490ff985223c7e963ede2
kibuchijw <kibuchijw@gmail.com>
2023-11-24 10:56:32 +0000

    Update (0-create_a_file.pp), syntax issues

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-file { 'tmp':
-  ensure  => 'present',
-  path    => '/tmp/school',
+# Create a file in tmp
+
+file { '/tmp/school':
+  ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
