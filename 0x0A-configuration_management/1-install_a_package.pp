c88482e3a8de72051e2677cd671b90695c9b78cf
kibuchijw <kibuchijw@gmail.com>
2023-11-24 13:19:08 +0000

    Refactor (1-install_a_package.pp), to check for pip3 first

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask 2.1.0 from pip3
 
-package { 'flask' :
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
 }
