0bb7902435a830fc96f766f29364e77d5e29cf5b
kibuchijw <kibuchijw@gmail.com>
2024-01-16 19:22:10 +0300

    Add 1-top_ten.py,  a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+    - subreddit (str): The name of the subreddit.
+
+    Returns:
+    - None: If the subreddit is not valid
+    or if there's an issue with the API request.
+    """
+    # Reddit API endpoint for getting subreddit posts
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    # Set a common User-Agent to avoid Too Many Requests errors
+    headers = {
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                    Chrome/58.0.3029.110 Safari/537.3"}
+
+    try:
+        # Make the API request
+        response = requests.get(
+            url, headers=headers, allow_redirects=False, timeout=30)
+
+        # Check if the request was successful and not a redirect
+        if response.status_code == 200 and not response.is_redirect:
+            # Parse JSON response and extract post titles
+            data = response.json()
+            # Extract the first 10 posts
+            posts = data["data"]["children"][:10]
+            for post in posts:
+                title = post["data"]["title"]
+                print(title)
+        else:
+            # Invalid subreddit or another issue
+            print(None)
+    except requests.RequestException as e:
+        # Handle any exceptions (e.g., network issues)
+        print(f"Error: {e}")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -24,3 +24,11 @@
   - Prototype: `def number_of_subscribers(subreddit)`
   - If not a valid subreddit, return 0.
   - NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
+
+### 1. Top Ten
+
+- Write a function that queries the [Reddit API](https://www.reddit.com/dev/api/) and prints the titles of the first 10 hot posts listed for a given subreddit.
+- Requirements:
+  - Prototype: `def top_ten(subreddit)`
+  - If not a valid subreddit, print None.
+  - NOTE: Invalid subreddits may return a redirect to search results. Ensure that you are not following redirects.
