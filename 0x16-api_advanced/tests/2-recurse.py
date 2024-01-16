596b1ed6d9dc5299bd3eed9b2d35e952a990da4f
kibuchijw <kibuchijw@gmail.com>
2024-01-16 22:38:07 +0300

    Refactor 2-recurse.py

--- 0x16-api_advanced/tests/2-recurse.py
+++ 0x16-api_advanced/tests/2-recurse.py
@@ -1,57 +1,49 @@
 #!/usr/bin/python3
-"""Function to query a list of all hot posts
-on a given Reddit subreddit.
+"""
+Module that queries the Reddit API and returns a list
+of all hot articles for a given subreddit.
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-    Returns a list of titles of all hot posts on a given subreddit.
+def recurse(subreddit, hot_list=[], after=None):
+    """Function that recursively queries the Reddit API and
+    returns a list of all hot articles.
 
     Args:
-    - subreddit (str): The name of the subreddit.
-    - hot_list (list): The list to store the titles of hot posts.
-    - after (str): The identifier of the last post in the current page.
-    - count (int): The running count of posts.
+        subreddit (str): The name of the subreddit.
+        hot_list (list): The list to store the titles of hot articles.
+        after (str): The identifier of the last post in the current page.
 
     Returns:
-    - list or None: A list containing the titles of all hot posts
-    for the subreddit, or None if the subreddit is not valid.
+        list or None: A list containing the titles of all hot articles
+        for the subreddit, or None if there's an issue with the API request.
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    # Set a common User-Agent to avoid Too Many Requests errors
     headers = {
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                 AppleWebKit/537.36 (KHTML, like Gecko)\
                     Chrome/58.0.3029.110 Safari/537.3"}
     params = {
-        "after": after,
-        "count": count,
-        "limit": 100
+        "limit": 100,
+        "after": after
     }
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=headers,
-                                params=params, allow_redirects=False)
+    if response.status_code == 200:
+        output = response.json()
+        hot_posts = output['data']['children']
+        after = output['data']['after']
+        for post in hot_posts:
+            hot_list.append(post.get('data').get('title'))
 
-        # Check if the subreddit is not valid (status code 404)
-        if response.status_code == 404:
-            return None
-
-        results = response.json().get("data")
-        after = results.get("after")
-        count += results.get("dist")
-
-        # Extract titles of hot posts
-        for c in results.get("children"):
-            hot_list.append(c.get("data").get("title"))
-
-        # Check if there are more pages and recurse with the next page's data
+        # Check if there are more pages, recurse with the next page's data
         if after is not None:
-            return recurse(subreddit, hot_list, after, count)
+            recurse(subreddit, hot_list, after)
 
         return hot_list
-    except requests.RequestException as e:
-        # Handle other exceptions (e.g., network issues)
-        return None
+    return None
