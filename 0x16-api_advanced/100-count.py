8face0379d7250ab12222520ee3a3ca4ebf169c3
kibuchijw <kibuchijw@gmail.com>
2024-01-17 07:33:29 +0300

    Update 100-count.py, adding docstring

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A recursive function that queries the Reddit API,
+parses the title of all hot articles, and prints
+a sorted count of given keywords (case-insensitive, delimited by spaces
+"""
 from collections import Counter
 import requests
 
